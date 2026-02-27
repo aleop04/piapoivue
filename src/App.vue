2b1ddb74219c1290394e8d3adf0cda557a41e01d
aleop04 <alejandra.perezovalle04@gmail.com>
2026-02-27 12:25:41 -0600

    Tailwind agregado chill y cambio en app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<div class="container mx-auto">
+  <h1 class="text-3xl font-bold underline text-center">helloooooo con tailwind css</h1>
+  <button class="bg-blue-500 hover:bg-blue-700 text -white font-bold py-2 px-4 rounded">
+    boton prueba
+  </button>
+</div>
 </template>
 
 <style scoped>
