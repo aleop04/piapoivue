95344e74744ab268db71bbc26d2f12c3acd0d34e
aleop04 <alejandra.perezovalle04@gmail.com>
2026-03-06 10:11:49 -0600

    bold

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template> 
   <footer class="bg-[#FF7608] text-center p-4 mt-10 font-[Nunito_Sans]">
-    <p class="text-white text-sm font-bold">
+    <p class="text-white text-lg font-bold">
       &copy; 2026 Destinario. Todos los derechos reservados.
     </p>
   </footer> 
