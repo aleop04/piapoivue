82312c364d19e0c860afa534df74e4db2c56060b
Maria <dulcerios674@gmail.com>
2026-03-05 22:13:03 -0600

    Pantallas: Intro, Inicio y Principal

---
+++ src/views/pantallaprincipal.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import BarraBusqueda from '@/components/BarraBusqueda.vue'
+import BotonBase from '@/components/BotonBase.vue'
+import CardResena from '@/components/CardResena.vue'
+import CardLugar from '@/components/CardLugar.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+
+<template>
+
+    <section class="bg-[#FFEBC9]">
+        <!--Barra de búsqueda-->
+        <div>
+            <BarraBusqueda />
+            <main>
+                <slot />
+            </main>
+        </div>
+
+        <div class="items-center max-w-9xl mx-auto px-4 p-5">
+            <!--Recompensas-->
+            <div
+                class="flex items-center justify-between bg-[#FF7608] text-white p-8 rounded-[2rem] font-[Nunito] h-[180px]">
+                <div>
+                    <p class="items-center justify-center text-xl">Hola, Usuario</p>
+                </div>
+                <div class="block">
+                    <p class="items-center justify-center text-xl">Mis puntos</p>
+                    <p class="items-center justify-center font-[Nunito] text-5xl font-bold">100</p>
+                </div>
+                <div>
+                    <button class="text-[#604646] bg-[#FFEBC9] font-[Nunito] text-xl py-3 px-6 rounded-full text-xl transition-all 
+                shadow-md active:scale-95 transition-transform duration-300 hover:scale-110">Ver recompensas</button>
+                </div> <!--*Cambiar color default-->
+            </div>
+
+            <br>
+
+            <!--Mejores reseñas-->
+            <div class="m-5 p-5">
+                <h2 class="font-[Nunito] text-[#604646] text-2xl">Mejores reseñas</h2>
+                <div class="flex items-center gap-5 mt-5 m-5 p-5">
+                    <CardResena imagen="/imagen piramide.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0"
+                        contenido="Excelente lugar para visitar." />
+                    <CardResena imagen="/imagen piramide.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0"
+                        contenido="Excelente lugar para visitar." />
+                    <CardResena imagen="/imagen piramide.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0"
+                        contenido="Excelente lugar para visitar." />
+                    <CardResena imagen="/imagen piramide.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0"
+                        contenido="Excelente lugar para visitar." />
+                </div>
+            </div>
+
+            <!--Lugares-->
+            <div class="m-5 p-5">
+                <h2 class="font-[Nunito] text-[#604646] text-2xl">Explorar</h2>
+                <div class="flex items-center gap-5 mt-5 m-5 p-5">
+                    <CardLugar imagen="/imagen cenote.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0" />
+                    <CardLugar imagen="/imagen cenote.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0" />
+                    <CardLugar imagen="/imagen cenote.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0" />
+                    <CardLugar imagen="/imagen cenote.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0" />
+                    <CardLugar imagen="/imagen cenote.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0" />
+                    <CardLugar imagen="/imagen cenote.jpg" titulo="Lugar" ubicacion="Ciudad" puntuacion="5.0" />
+                </div>
+            </div>
+
+        </div>
+        <br>
+        <Footer />
+    </section>
+</template>
--- src/views/pantallauno.vue
+++ src/views/pantallauno.vue
@@ -1,32 +1,74 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+
+import BotonBase from '@/components/BotonBase.vue'
+
+const showLogin = ref(false)
 </script>
 
 <template>
-  <section class="relative h-screen w-full overflow-hidden">
+  <section class="relative h-screen w-full overflow-hidden flex items-center justify-center">
 
     <!-- foto fondo -->
-  <div 
-      class="absolute inset-0 bg-cover bg-center"
-      style="background-image: url('/fotofondo.jpg');"
-    ></div>
+    <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('/fotofondo.jpg');"></div>
 
     <!-- naranja encima -->
     <div class="absolute inset-0 bg-[#FF7608]/70"></div>
 
-    <!-- logo destinario + mini texto -->
-  <div class="relative z-10 flex flex-col items-center justify-center min-h-screen text-center gap-3">
-    <img 
-      src="/destinariologo3.png" 
-      alt="Destinario"
-      class="w-[750px]"
-    />
+    <!--logo ave-->
+    <div class="absolute top-8 left-8 z-20">
+      <img src="/public/destinariologo1.png" alt="Logo" class="w-25 h-25" />
+    </div>
 
-    <p class="font-['Asul'] text-white text-2xl md:text-4xl font-normal text-center max-w-4xl">
-        Las mejores guías turísticas para visitar en México durante el mundial
-        de futbol 2026.
-    </p>
-  </div>
+    <Transition name="fade" mode="out-in">
 
+      <div v-if="!showLogin" key="welcome" class="relative min-h-screen flex flex-col items-center justify-center text-center gap-6 pt-10">
+        <img src="/destinariologo3.png" alt="Destinario" class="w-[600px] md:w-[750px]" />
+
+        <p class="font-['Asul'] text-white text-2xl md:text-4xl max-w-4xl px-4">
+          Las mejores guías turísticas para visitar en México durante el mundial de fútbol 2026.
+        </p>
+
+        <BotonBase text="Iniciar Sesión"  @click="showLogin = true" />
+
+        <div class="font-[Nunito] absolute bottom-0 bg-[#FFEBC9] py-12 px-40 rounded-t-3xl">
+          <p class="text-[#604646] text-xl">
+            ¿No tienes una cuenta? <br>
+            <router-link to="/registro" class="font-bold underline">Regístrate aquí</router-link>
+          </p>
+        </div>
+      </div>
+
+      <!--Form inicio de sesion-->
+      
+      <div v-else key="login"
+        class="relative z-10 bg-[#FFEBC9] p-10 rounded-[40px] shadow-xl flex flex-col items-center w-[90%] max-w-[600px]">
+        <img src="/destinariologo2.png" alt="Destinario" class="max-w-[300px] md:max-w-[400px] mb-2" />
+        <br>
+        <form class="w-full flex flex-col gap-5 font-[Nunito] text-xl">
+          <div class="flex flex-col" >
+            <label class="text-[#604646] font-bold text-center mb-1">Correo electrónico</label>
+            <input type="email" placeholder="Correo"
+              class="rounded-full border-none bg-[#FDF0D9] p-3 text-center outline-none placeholder:text-[#604646]/50 shadow-sm">
+          </div>
+
+          <div class="flex flex-col">
+            <label class="text-[#604646] font-bold text-center mb-1">Contraseña</label>
+            <input type="password" placeholder="Contraseña"
+              class="rounded-full border-none bg-[#FDF0D9] p-3 text-center outline-none placeholder:text-[#604646]/50 shadow-sm">
+          </div>
+
+          <router-link to="/principal" class="flex justify-center w-full"> <!--navegacion por ahora-->
+            <BotonBase text="Iniciar Sesión" />
+          </router-link>
+        </form>
+
+        <br>
+        <button @click="showLogin = false" class="font-[Nunito] text-xs mt-4 text-[#604646] underline" >¿No tienes una cuenta? Regístrate
+          aquí</button>
+      </div>
+
+    </Transition>
   </section>
 </template>
